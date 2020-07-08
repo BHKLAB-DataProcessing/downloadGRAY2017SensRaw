9e27d9290776eeb78354aa42fee269c0677e0440
Petr Smirnov <psmirnov2000@gmail.com>
2020-07-08 17:01:42 -0400

    rewrite + fix units for 2c4

--- downloadSensData.R
+++ downloadSensData.R
@@ -1,99 +1,96 @@
-library(PharmacoGx)
-library(openxlsx)
+require(data.table) || install.packages("data.table")
+require(matrixStats) || install.packages("matrixStats")
+library(abind)
+
+concentrations.no <- 9
 
 options(stringsAsFactors=FALSE)
+gray.raw.drug.sensitivity <- fread(file="./Gray_data_raw_dose_response.csv")
+gray.conc <- fread("./Gray_drug_conc.csv")
+
+gray.raw.drug.sensitivity[,
+      background := rowMedians(data.matrix(gray.raw.drug.sensitivity[,grep("^background_od", 
+                                                                           colnames(gray.raw.drug.sensitivity),
+                                                                            v=TRUE), with=FALSE]))]
+
+gray.raw.drug.sensitivity[,
+      ctrl := rowMedians(data.matrix(gray.raw.drug.sensitivity[,grep("^od0\\.", 
+                                                                      colnames(gray.raw.drug.sensitivity),
+                                                                       v=TRUE), with=FALSE])) - background]
+
+
+for( i in seq_len(concentrations.no)){
+
+      curCol <- paste0("response", i)
+
+      curDoseCol <- grep(paste0("^od", i, "\\."), colnames(gray.raw.drug.sensitivity))
+      gray.raw.drug.sensitivity[,
+        (curCol) := (rowMedians(data.matrix(gray.raw.drug.sensitivity[,curDoseCol, 
+                                                                      with=FALSE])) - background)/ctrl * 100]
+
+}
+
+gray.raw.drug.sensitivity <- gray.raw.drug.sensitivity[,-grep("^od", 
+                                                              colnames(gray.raw.drug.sensitivity), 
+                                                              v=TRUE), with=FALSE]
+
+# gray.raw.drug.sensitivity <- gray.raw.drug.sensitivity[,-grep("*background*", 
+#                                                               colnames(gray.raw.drug.sensitivity), 
+#                                                               v=TRUE), with=FALSE]
+# gray.raw.drug.sensitivity <- gray.raw.drug.sensitivity[,-c("T0_median_od")]
+
+
+
+gray.raw.drug.sensitivity <- merge(x=gray.raw.drug.sensitivity, 
+                                   y=gray.conc, by=c("drug_group_id", "drug"), all.x=TRUE)
+
+
+
+gray.raw.drug.sensitivity[,exp_id := paste(drug, cellline, ID.x, sep="_")]
+
+conc.cols <- grep("^c[0-9]+", colnames(gray.raw.drug.sensitivity), v=TRUE)
+
+gray.raw.drug.sensitivity[,(conc.cols) := lapply(.SD, function(x) return(x*1e6)), .SDcols = conc.cols]
+
+gray.raw.drug.sensitivity[drug=="2C4", (conc.cols) := .SD/388.8,.SDcols=conc.cols] ## molecular weight from Pubchem, retrieved Jul 8, 2020
+gray.raw.drug.sensitivity[drug=="2C4", units:="M"] # removing one drug in mg/ml for now. 
+
+
+                                                 
+# gray.conc.m <- data.table::melt(gray.raw.drug.sensitivity[,c("exp_id", conc.cols), with=FALSE], id.vars="exp_id")                                                 
+                                                 
+# colnames(gray.conc.m) <- c("exp_id", "DoseNum", "DoseValue") 
+
+# gray.conc.m[,DoseNum := gsub(DoseNum, pat="c", rep="")]
+   
+response.cols <- grep("response", x=colnames(gray.raw.drug.sensitivity), v=TRUE)
+
+raw.sensitivity <- abind(data.matrix(gray.raw.drug.sensitivity[,..conc.cols]), data.matrix(gray.raw.drug.sensitivity[,..response.cols]), along=3)
+
+rownames(raw.sensitivity) <- gray.raw.drug.sensitivity[["exp_id"]]
+
+colnames(raw.sensitivity) <- paste0("dose", seq_len(concentrations.no))
+
+dimnames(raw.sensitivity)[[3]] <- c("Dose", "Viability")
+
+sensitivity.info <- as.data.frame(gray.raw.drug.sensitivity[,.(drug, cellline, drug_plate_id, T0_plate_id, T0_median_od, T0_background_od1, T0_background_od2, units)])
+colnames(sensitivity.info)[1:2] <- c("cellid", "drugid")
+sensitivity.info$nbr.conc.tested <- 9
+sensitivity.info$min.Dose.uM <- apply(raw.sensitivity[,,"Dose"], 1, min)
+sensitivity.info$max.Dose.uM <- apply(raw.sensitivity[,,"Dose"], 1, max)
+
+save(raw.sensitivity, sensitivity.info, file="/pfs/out/drug_norm_post.RData")
 
 ##create sensitivity slices
-    
-    getGRAYrawData <-
-      function(result.type=c("array", "list")){
-        
-        gray.raw.drug.sensitivity <- read.csv(file="/pfs/downloadGRAY2017SensRaw/Gray_data_raw_dose_response.csv", header=T, row.names=1, stringsAsFactors=FALSE)
-        gray.raw.drug.sensitivity.list <- do.call(c, apply(gray.raw.drug.sensitivity, 1, list))
-        
-        gray.conc <- read.csv(file="/pfs/downloadGRAY2017SensRaw/Gray_drug_conc.csv", header=T, row.names=1, stringsAsFactors=FALSE)
-        
-        concentrations.no <- 9
-        
-        if(result.type == "array"){
-          ## create the gray.drug.response object including information viablilities and concentrations for each cell/drug pair
-          obj <- array(NA, dim=c(length(unique(gray.raw.drug.sensitivity[ , "cellline"])), length(unique(gray.raw.drug.sensitivity[ , "drug"])), 2, concentrations.no), dimnames=list(unique(gray.raw.drug.sensitivity[ , "cellline"]), unique(gray.raw.drug.sensitivity[ , "drug"]), c("concentration", "viability"), 1:concentrations.no))
-        }
-        fnexperiment <-
-          function(values){
-            cellline <- values["cellline"]
-            drug <- values["drug"]
-            values["drug_group_id"] <- gsub(" ", "", values["drug_group_id"]) #removes space from drug-id values -i.e. (" 50") - which was causing some values AUC/IC50 values to not be matched
-            doses <- as.numeric(gray.conc[which(gray.conc[,"drug_group_id"] == values["drug_group_id"] & gray.conc[,"drug"] == values["drug"]), grep("^c", colnames(gray.conc))]) * 10 ^ 6 # micro molar
-            
-            if(concentrations.no > length(doses)) {doses <- c(doses, rep(NA, concentrations.no - length(doses)))}
-            
-            #responses <- as.numeric(unlist(strsplit(input.matrix["Activity Data\n(raw median data)"], split=",")))  #nature paper raw data
-            
-            responses <- NULL
-            background <- median(as.numeric(values[grep("^background_od", names(values))]))
-            ctrl <- median(as.numeric(values[sprintf("od%s.%s",0,1:3)])) - background
-            ctrl <- ifelse(ctrl < 0, 1, ctrl)
-            for( i in 1:concentrations.no)
-            {
-              res <- median(as.numeric(values[sprintf("od%s.%s",i, 1:3)])) - background
-              res <- ifelse(res < 0, 0, res)
-              responses <- c(responses, res/ctrl)
-            }
-            responses <- responses * 100
-            if(result.type == "array"){
-              obj[cellline,drug, "concentration", 1:length(doses)] <<- doses
-              obj[cellline,drug, "viability", 1:length(responses)] <<- responses
-            }else{
-              return(list(cell=cellline, drug=drug, doses=doses, responses=responses))#paste(doses, collapse = ","), responses=paste(responses, collapse = ",")))
-            }
-          }
-        
-        gray.raw.drug.sensitivity.res <- mapply(fnexperiment, values=gray.raw.drug.sensitivity.list)
-        if(result.type == "array"){
-          return(list("data"=obj, "concentrations.no"=concentrations.no))
-        }else{
-          return(list("data"=gray.raw.drug.sensitivity.res, "concentrations.no"=concentrations.no))
-        }
-      }
-    raw.sensitivity <- getGRAYrawData(result.type="list")
-    
-    
-    con_tested <- raw.sensitivity$concentrations.no
-    raw.sensitivity <- t(raw.sensitivity$data)
-    raw.sensitivity <- t(apply(raw.sensitivity,1, function(x){unlist(x)}))
-    
-    rownames(raw.sensitivity)  <- sprintf("drugid_%s_%s",as.character(raw.sensitivity[ ,2]),as.character(raw.sensitivity[ ,1]))
-    ## handle replicates
-    tt <- rownames(raw.sensitivity)
-    for(i in 1:length(tt)) {
-      xx <- which(tt == tt[i])
-      if(length(xx) > 1) {
-        for(j in 1:length(xx)) {
-          tt[xx[j]] <- paste(tt[xx[j]], j, sep="_")
-        }
-      }
-    }
-    rownames(raw.sensitivity) <- tt
-    
-    tt <- paste0("doses", con_tested)
-    sensitivity.info <- raw.sensitivity[ , c(1, 2, 3, grep(tt, colnames(raw.sensitivity)))]
-    colnames(sensitivity.info) <- c("cellid", "drugid", "min.Dose.uM", "max.Dose.uM")
-    sensitivity.info <- cbind(sensitivity.info, "nbr.conc.tested"=con_tested)
-    raw.sensitivity <- raw.sensitivity[ ,-c(1,2)]
-    raw.sensitivity <- array(c(as.matrix(as.numeric(raw.sensitivity[ ,1:con_tested])), as.matrix(as.numeric(raw.sensitivity[ ,(con_tested+1):(2*con_tested)]))), c(nrow(raw.sensitivity), con_tested, 2),
-                         dimnames=list(rownames(raw.sensitivity), colnames(raw.sensitivity[ ,1:con_tested]), c("Dose", "Viability")))
-
-    save(raw.sensitivity, sensitivity.info, tt, con_tested, file="/pfs/out/drug_norm_post.RData")
 
 
-    raw.sensitivity.x <- parallel::splitIndices(nrow(raw.sensitivity), floor(nrow(raw.sensitivity)/1000))
+raw.sensitivity.x <- parallel::splitIndices(nrow(raw.sensitivity), floor(nrow(raw.sensitivity)/1000))
 
-    dir.create("/pfs/out/slices/")
+dir.create("/pfs/out/slices/")
 
-    for(i in seq_along(raw.sensitivity.x)){
+for(i in seq_along(raw.sensitivity.x)){
 
-      slce <- raw.sensitivity[raw.sensitivity.x[[i]],,]
-      saveRDS(slce, file=paste0("/pfs/out/slices/gray2017_raw_sens_", i, ".rds"))
+  slce <- raw.sensitivity[raw.sensitivity.x[[i]],,]
+  saveRDS(slce, file=paste0("/pfs/out/slices/gray2017_raw_sens_", i, ".rds"))
 
-    }
+}
